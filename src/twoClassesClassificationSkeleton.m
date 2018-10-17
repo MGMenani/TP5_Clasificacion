45468aa02040d8b1787e9c4fba8a8d8b84028638
Alejandro <alee_pq@hotmail.com>
2018-10-16 22:00:24 -0600

    Grafica fail

--- src/twoClassesClassificationSkeleton.m
+++ src/twoClassesClassificationSkeleton.m
@@ -38,8 +38,7 @@ function twoClassesClassificationSkeleton
                 T(t_i) = 1;
                 t_i = t_i + 1;
             end
-            
-            
+                  
         end
     end
     %T is a column vector
@@ -68,14 +67,64 @@ function twoClassesClassificationSkeleton
     
     W = getW_leastSquares(Xd, T); %Coordenada de cada X con un bias
     %test 1
-    for i = 1:size(C1, 1)
-        yResC1(i) = getY(W, [1 C1(i, :)]);
+    for i = 1:size(C1test, 1)
+        yResC1(i) = getY(W, [1 C1test(i, :)]);
     end
     
-    for i = 1:size(C2, 1)
-        yResC2(i) = getY(W, [1 C2(i, :)]);
+    for i = 1:size(C2test, 1)
+        yResC2(i) = getY(W, [1 C2test(i, :)]);
     end
-        
+    
+    
+    %GRAFICACION DEL EXITO DEL MODELO
+    
+    figure;
+    %int8(length(yResC2)+length(yResC1)), 
+    datos = [int8(length(yResC1)),int8(length(yResC2)); sum(int8(yResC1)==0) , sum(int8(yResC2)==1);
+             sum(int8(yResC1)==1) , sum(int8(yResC2)==0)];
+    bar(datos , 'stacked'); %,  sum(int8(yResC2)==1)] );
+    legendc1 = 'Clase 1'; legendc2 = 'Clase 2'; 
+    legend(legendc1, legendc2);
+    set(gca,'xticklabel',{'Total muestras','Aciertos', 'Fallos'});
+
+    
+    %GRAFIACION DE LA SUPERFICIE DE DECISION
+
+%     z = [yResC1 yResC2];
+%     z = double(z');
+%     c1c2 = [C1test; C2test];
+%     xyz = [c1c2, z];
+%     
+%     figure;
+%     scatter3(xyz(:,1),xyz(:,2),xyz(:,3));
+%     hold on;
+%     
+%     
+%     for i = 1:size(C1test, 1)
+%         Zc1(i) = W' * [1 C1test(i, :)]';
+%     end
+%     
+%     for i = 1:size(C2test, 1)
+%         Zc2(i) = W' * [1 C2test(i, :)]';
+%     end
+%     
+%     Z = [Zc1 Zc2];
+%     
+%     surf(Z);
+%     
+%     
+%     f = fit( c1c2, z, 'poly23' );
+%     figure;
+%     plot(f, c1c2, z);
+%     
+%     weights = [ 0.3400 ,-0.0553 , -0.0667];
+%     [x1,x2]=ndgrid(-5:1:5,-5:1:5);
+%     y = weights(1) + weights(2)*x1 + weights(3)*x2
+%     
+%     figure;
+%     surf(x1,x2,y);
+    
+
     %---------------------------------------------------------------------
     
     %prueba con fisher 
@@ -117,7 +166,12 @@ function W = perceptronTraining(C1n, C2n, numIter)
 end
 %activation function of the perceptron algorithm
 function f = perceptronActivationFunc(W, x) 
-   
+   r = W'*x;
+   if r>=0
+       f = 1
+   else
+       f = -1
+   end
 end
 
 %C1 and C2 tagged data
